d13fd0ad8e3637f4f4c7f27a9dedc4c1c9a6432c
CharlesFan <rock9030@gmail.com>
2025-04-01 16:37:38 +0800

    server init

---
+++ cmd/app/router.go
@@ -0,0 +1,35 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type RequestHeader struct {
+	// TODO authorization
+	//Authorization string `json:"authorization"`
+}
+
+type Router struct {
+	addr   string
+	router *gin.Engine
+}
+
+func NewRouter(addr string) *Router {
+	return &Router{
+		addr:   addr,
+		router: gin.Default(),
+	}
+}
+
+func (r *Router) Config() {
+	r.router.MaxMultipartMemory = 8 << 20 // 8 MiB
+
+	v := r.router.Group("/sandbox")
+	v.Any("", func(c *gin.Context) {
+		c.String(200, "power by Charles")
+	})
+}
+
+func (r *Router) Run() {
+	r.router.Run(r.addr)
+}
---
+++ cmd/app/server.go
@@ -0,0 +1,55 @@
+package app
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/charlesfan/task-go/entity/config"
+)
+
+var server = newServer()
+
+type Server struct {
+	router *Router
+}
+
+func (s *Server) Run(c config.Config) error {
+	s.router = NewRouter(net.JoinHostPort(c.Server.Host, c.Server.Port))
+	s.router.Config()
+	s.router.Run()
+	return nil
+}
+
+func newServer() *Server {
+	return &Server{}
+}
+
+func NewServerCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:          "server",
+		Long:         `Here is task-go backend server.`,
+		SilenceUsage: true,
+		Run: func(cmd *cobra.Command, args []string) {
+			config.Init()
+			c := config.New()
+			server.Run(c)
+		},
+		Args: func(cmd *cobra.Command, args []string) error {
+			for _, arg := range args {
+				if len(arg) > 0 {
+					return fmt.Errorf("%q does not take any arguments, got %q", cmd.CommandPath(), args)
+				}
+			}
+			return nil
+		},
+	}
+
+	cmd.Flags().StringP("config", "c", "", "config file (default is $HOME/.cobra.yaml)")
+
+	viper.BindPFlags(cmd.Flags())
+
+	return cmd
+}
