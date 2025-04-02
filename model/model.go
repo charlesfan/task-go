1d7983f75570530146d6b28659a28b722ce1b636
CharlesFan <rock9030@gmail.com>
2025-04-02 10:37:29 +0800

    feature(model): add json marshal&unmarshal method

--- model/model.go
+++ model/model.go
@@ -1,6 +1,29 @@
 package model
 
+import (
+	"encoding/json"
+)
+
 type NullInt struct {
 	Int   int
 	Valid bool
+}
+
+func (ni *NullInt) UnmarshalJSON(data []byte) error {
+	var temp int
+	if err := json.Unmarshal(data, &temp); err == nil {
+		ni.Int = temp
+		ni.Valid = true
+		return nil
+	}
+
+	ni.Valid = false
+	return nil
+}
+
+func (ni *NullInt) MarshalJSON() ([]byte, error) {
+	if ni.Valid {
+		return json.Marshal(ni.Int)
+	}
+	return json.Marshal(nil)
 }
